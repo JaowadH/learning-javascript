083a9a3e3b4dfd078f505552d23df1833616cb11
Jaowad Hossain <jaowadh@outlook.com>
2026-02-28 13:35:39 -0330

    Initial commit, first bit of javascript with basic HTML/CSS

---
+++ website/index.js
@@ -0,0 +1,10 @@
+//console.log(`Hello`);
+//console.log(`I like pizza`);
+
+//window.alert(`This is an alart`);
+//window.alert(`I like Pizza`);
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I like pizza`;
+
+// This is a comment
