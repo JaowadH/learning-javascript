4990faaccf4fba88f069518f0b905db864c0c3a1
Jaowad Hossain <jaowadh@outlook.com>
2026-02-28 17:22:54 -0330

    All veriable example in one

--- website/index.js
+++ website/index.js
@@ -1,18 +1,10 @@
 // variable = A container that stores a value.
 //                   Behaves as if it were the value it contains.
 
-//1. declaration let x;
-//2. assignment x=100;
+let fullName = "Jaowad Hossain";
+let age = 25;
+let isStudent = false;
 
-//let age = 25;
-//let price = 10.99;
-//let gpa = 2.1;
-
-let firstName = "Jaowad";
-let favouriteFood = "Pizza";
-let email = "jaowad@gmail.com"
-
-console.log(typeof firstName);
-console.log(`Your name is ${firstName}`);
-console.log(`You like ${favouriteFood}`);
-console.log(`Your email is ${email}`);
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
