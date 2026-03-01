13af095add4720653edcce8ece87b0ff645ed5de
Jaowad Hossain <jaowadh@outlook.com>
2026-03-01 12:31:43 -0330

    Basic counter

--- website/index.js
+++ website/index.js
@@ -1,14 +1,19 @@
-// const = a variable that can't be changed
+// COUNTER PROGRAM
+const increaseBtn = document.getElementById("increaseBtn");
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
 
-const PI = 3.14159;
-let radius;
-let circumference;
-
-// PI = 420.69;
-
-document.getElementById("mySubmit").onclick = function(){
-    radius = document.getElementById("myText").value;
-    radius = Number(radius);
-    circumference = 2 * PI * radius;
-    document.getElementById("myH3").textContent = circumference + "cm";
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}
+decreaseBtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+resetBtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = count;
 }
