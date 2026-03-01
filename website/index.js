65089001300d51e65a3b2fdcae4817a2dfafd090
Jaowad Hossain <jaowadh@outlook.com>
2026-03-01 11:55:16 -0330

    basic constant operation

--- website/index.js
+++ website/index.js
@@ -1,17 +1,14 @@
-// How to accept user input
+// const = a variable that can't be changed
 
-// 1. EASY WAY = window prompt
-// 2. PROFESSIONAL WAY = HTML textbox
+const PI = 3.14159;
+let radius;
+let circumference;
 
-// ------------------------ EASY WAY ------------------------
-// let username = window.prompt("What's your username?");
-
-// console.log(username);
-
-// -------------------- PROFESSIONAL WAY --------------------
-let username;
+// PI = 420.69;
 
 document.getElementById("mySubmit").onclick = function(){
-    username = document.getElementById("myText").value;
-    document.getElementById("myH1").textContent = `Hello ${username}`
+    radius = document.getElementById("myText").value;
+    radius = Number(radius);
+    circumference = 2 * PI * radius;
+    document.getElementById("myH3").textContent = circumference + "cm";
 }
